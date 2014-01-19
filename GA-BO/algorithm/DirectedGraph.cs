ebe7c2608e6e328577916a6ac404624f32bb1a50
potoczny <natalia.potoczny@gmail.com>
2014-01-18 18:11:19 -0800

    zaimplementowane IslandConnections

---
+++ GA-BO/algorithm/DirectedGraph.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GA_BO.algorithm
+{
+    class DirectedGraph
+    {
+        private Island[] _islands;
+        private List<int>[] _connections;
+
+        public DirectedGraph(Island[] islands, List<int>[] connection)
+        {
+            this._islands = islands;
+            this._connections = connection;
+        }
+
+        public List<Island> GetConnections(Island island)
+        {
+            if (!_islands.Contains(island))
+            {
+                throw new Exception("Island does not exist");
+            }
+
+            int index = Array.IndexOf(_islands, island);
+
+            if (!_connections[index].Any())
+            {
+                return null;
+            }
+
+            List<Island> toReturn = new List<Island>();
+            foreach (int i in _connections[index])
+            {
+                toReturn.Add(_islands[i]);
+            }
+            return toReturn;
+        }
+    }
+}
--- GA-BO/algorithm/IslandConnections.cs
+++ GA-BO/algorithm/IslandConnections.cs
@@ -7,14 +7,41 @@ namespace GA_BO.algorithm
 {
     public class IslandConnections
     {
-        public IslandConnections(List<int>[] connection,Island[] islands)
+        DirectedGraph di_graph;
+        private bool _debugMode = true; //change to false if sure than args are correct
+
+        public IslandConnections(List<int>[] connection, Island[] islands)
         {
             // create digraf where every vertex is island[i], and connections corresponds to adjacence list given in 'connection'
+            
+            if (_debugMode == true)
+            {
+                if (connection.Length != islands.Length)
+                {
+                    throw new Exception("Both array args must have the same length");
+                }
+
+                for (int i = 0; i < connection.Length; i++)
+                {
+                    if (connection[i].Any(num => num >= connection.Length))
+                    {
+                        throw new Exception("Found wrong connection, index of island " + i + " out of range");
+                    }
+
+                    if (connection[i].Contains(i))
+                    {
+                        throw new Exception("Found wrong connection, island -> island for island = " + i);
+                    }
+                }
+            }
+
+            di_graph = new DirectedGraph(islands, connection);
         }
+
         public List<Island> getConnections(Island island)
         {
             //return all X adjacent to island (such as: island -> X)
-            return null;
+            return di_graph.GetConnections(island);
         }
 
     }
